8e6317f56f5941467a18c508ee61275c3c0745bd
Wissanukhong <wissanu.khong@gmail.com>
2020-01-13 22:40:19 +0700

    create same page

--- newsProject/app.js
+++ newsProject/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var menuRouter = require('./routes/menu') //newrouter
+var footerRouter = require('./routes/footer')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/menu', menuRouter);
+app.use('/footer', footerRouter)
 
 
 // catch 404 and forward to error handler
